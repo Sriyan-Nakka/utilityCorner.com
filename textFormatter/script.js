4d74cabc4c16ac019819d9b1bf84bdc425dc6eec
Sriyan-Nakka <sriyannakka@gmail.com>
2025-05-21 14:56:07 +0530

    version 1.3

--- textFormatter/script.js
+++ textFormatter/script.js
@@ -9,6 +9,7 @@ const strikethroughUnformatButton = document.getElementById(
   "strikethroughUnformatButton"
 );
 const italicFormatButton = document.getElementById("italicFormatButton");
+const italicUnformatButton = document.getElementById("italicUnformatButton");
 const underlineUnformatButton = document.getElementById(
   "underlineUnformatButton"
 );
@@ -21,14 +22,14 @@ let formattedText = document.getElementById("formattedText");
 let localValue = localStorage.getItem("formattedText");
 console.log(localStorage.getItem("formattedText"));
 
-function formattingTextContent() {
+function setText() {
   formattedText.innerHTML =
     theText.value || localStorage.getItem("formattedText");
   theText.innerHTML = theText.value || localStorage.getItem("formattedText");
 }
 
-function formatText() {
-  formattingTextContent();
+function applyStyles() {
+  setText();
   fontName = document.getElementById("fontName").value;
   fontSize = document.getElementById("fontSize").value;
   otherColor = document.getElementById("otherColor").value;
@@ -69,30 +70,65 @@ function formatText() {
     formattedText.style.textDecoration = "none";
   };
 
-  switch (true) {
-    case fontName == "Serif":
+  switch (fontName) {
+    case "Serif":
+      document.querySelector("#customFont").value = "";
+      document.querySelector("#customFontPara").style.display = "none";
       formattedText.style.fontFamily = "serif";
       break;
-    case fontName == "Sans Serif":
+    case "Sans Serif":
+      document.querySelector("#customFont").value = "";
+      document.querySelector("#customFontPara").style.display = "none";
       formattedText.style.fontFamily = "sans-serif";
       break;
-    case fontName == "Reddit Sans":
+    case "Reddit Sans":
+      document.querySelector("#customFont").value = "";
+      document.querySelector("#customFontPara").style.display = "none";
       formattedText.style.fontFamily = "Reddit Sans";
       break;
-    case fontName == "Posten One":
+    case "Posten One":
+      document.querySelector("#customFont").value = "";
+      document.querySelector("#customFontPara").style.display = "none";
       formattedText.style.fontFamily = "Posten One";
       break;
-    case fontName == "Raleway":
+    case "Raleway":
+      document.querySelector("#customFont").value = "";
+      document.querySelector("#customFontPara").style.display = "none";
       formattedText.style.fontFamily = "Raleway";
       break;
-    case fontName == "Ubuntu":
+    case "Ubuntu":
+      document.querySelector("#customFont").value = "";
+      document.querySelector("#customFontPara").style.display = "none";
       formattedText.style.fontFamily = "Ubuntu";
+      break;
+    case "Custom":
+      document.querySelector("#customFontPara").style.display = "block";
+
+      document.querySelector("#customFont").addEventListener("change", (e) => {
+        e.preventDefault();
+        let file = e.target.files[0];
+        let blobUrl = URL.createObjectURL(file);
+        let customFontName = "customFont_" + Date.now();
+
+        css = `
+        @font-face {
+          font-family: "${customFontName}";
+          src: url("${blobUrl}");
+        }
+          `;
+
+        let style = document.createElement("style");
+        style.textContent = css;
+        document.head.appendChild(style);
+
+        formattedText.style.fontFamily = customFontName;
+      });
       break;
   }
 
   localStorage.setItem("formattedText", formattedText.innerHTML);
 }
 
-document.addEventListener("DOMContentLoaded", formatText);
-document.addEventListener("keyup", formatText);
-document.addEventListener("click", formatText);
+document.addEventListener("DOMContentLoaded", applyStyles);
+document.addEventListener("keyup", applyStyles);
+document.addEventListener("click", applyStyles);
